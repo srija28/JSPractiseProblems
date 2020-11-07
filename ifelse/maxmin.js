3721766d958dfcd6572477b819e1e786c93915bb
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-11-07 18:55:43 +0530

    [Shreya]Added: ifelse condition practise problems

---
+++ ifelse/maxmin.js
@@ -0,0 +1,8 @@
+let firstNo = Math.floor(Math.random() * 1000);
+let secondNo = Math.floor(Math.random() * 1000);
+let thirdNo = Math.floor(Math.random() * 1000);
+let fourthNo = Math.floor(Math.random() * 1000);
+let fifthNo = Math.floor(Math.random() * 1000);
+
+console.log("Maximum of the five numbers :" + Math.max(firstNo,secondNo,thirdNo,fourthNo,fifthNo));
+console.log("Minimum of the five numbers :" + Math.min(firstNo,secondNo,thirdNo,fourthNo,fifthNo));
